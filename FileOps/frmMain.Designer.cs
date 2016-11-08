0ccb4824f1e494f0b85cad40aa0163b0bf4931fd
Jeremy Wiley <jwileyfl@msn.com>
2016-11-08 18:21:19 -0500

    Add project files.

---
+++ FileOps/frmMain.Designer.cs
@@ -0,0 +1,155 @@
+﻿namespace FileOps
+{
+    partial class frmMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.fsw1 = new System.IO.FileSystemWatcher();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.FileName = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.Activity = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.Time = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.Date = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.Size = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.Folder = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            this.btnFolder = new System.Windows.Forms.Button();
+            this.bw1 = new System.ComponentModel.BackgroundWorker();
+            ((System.ComponentModel.ISupportInitialize)(this.fsw1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // fsw1
+            // 
+            this.fsw1.EnableRaisingEvents = true;
+            this.fsw1.IncludeSubdirectories = true;
+            this.fsw1.Path = "C:\\";
+            this.fsw1.SynchronizingObject = this;
+            // 
+            // listView1
+            // 
+            this.listView1.AutoArrange = false;
+            this.listView1.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.Folder,
+            this.FileName,
+            this.Size,
+            this.Activity,
+            this.Date,
+            this.Time});
+            this.listView1.FullRowSelect = true;
+            this.listView1.Location = new System.Drawing.Point(21, 61);
+            this.listView1.MultiSelect = false;
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(527, 245);
+            this.listView1.TabIndex = 1;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.View = System.Windows.Forms.View.Details;
+            // 
+            // FileName
+            // 
+            this.FileName.Text = "File Name";
+            this.FileName.Width = 85;
+            // 
+            // Activity
+            // 
+            this.Activity.Text = "Activity";
+            this.Activity.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.Activity.Width = 65;
+            // 
+            // Time
+            // 
+            this.Time.Text = "Time";
+            // 
+            // Date
+            // 
+            this.Date.Text = "Date";
+            // 
+            // Size
+            // 
+            this.Size.Text = "Size";
+            this.Size.Width = 56;
+            // 
+            // Folder
+            // 
+            this.Folder.Text = "Folder";
+            this.Folder.Width = 175;
+            // 
+            // folderBrowserDialog1
+            // 
+            this.folderBrowserDialog1.Description = "Select a Folder to Monitor";
+            this.folderBrowserDialog1.RootFolder = System.Environment.SpecialFolder.MyComputer;
+            this.folderBrowserDialog1.ShowNewFolderButton = false;
+            // 
+            // btnFolder
+            // 
+            this.btnFolder.BackColor = System.Drawing.Color.LightSteelBlue;
+            this.btnFolder.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnFolder.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btnFolder.Font = new System.Drawing.Font("Arial Unicode MS", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnFolder.Location = new System.Drawing.Point(25, 16);
+            this.btnFolder.Name = "btnFolder";
+            this.btnFolder.Size = new System.Drawing.Size(111, 28);
+            this.btnFolder.TabIndex = 2;
+            this.btnFolder.Text = "Select Folder";
+            this.btnFolder.UseVisualStyleBackColor = false;
+            this.btnFolder.Click += new System.EventHandler(this.btnFolder_Click);
+            // 
+            // bw1
+            // 
+            this.bw1.WorkerReportsProgress = true;
+            this.bw1.WorkerSupportsCancellation = true;
+            // 
+            // frmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
+            this.ClientSize = new System.Drawing.Size(584, 321);
+            this.Controls.Add(this.btnFolder);
+            this.Controls.Add(this.listView1);
+            this.Name = "frmMain";
+            this.Text = "Folder Watch";
+            ((System.ComponentModel.ISupportInitialize)(this.fsw1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.IO.FileSystemWatcher fsw1;
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.ColumnHeader Folder;
+        private System.Windows.Forms.ColumnHeader FileName;
+        private System.Windows.Forms.ColumnHeader Size;
+        private System.Windows.Forms.ColumnHeader Activity;
+        private System.Windows.Forms.ColumnHeader Date;
+        private System.Windows.Forms.ColumnHeader Time;
+        private System.Windows.Forms.Button btnFolder;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+        private System.ComponentModel.BackgroundWorker bw1;
+    }
+}
+
---
+++ FileOps/frmMain.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Diagnostics;
+using System.Windows.Forms;
+
+namespace FileOps
+{
+    public partial class frmMain : Form
+    {
+        string strPath = "";
+
+        public frmMain()
+        {
+            InitializeComponent();
+
+        }
+
+        private void btnFolder_Click(object sender, EventArgs e)
+        {
+            folderBrowserDialog1.ShowDialog();
+            strPath = folderBrowserDialog1.SelectedPath.ToString();
+            Debug.Print(strPath);
+
+            listView1.Clear();
+            fsw1.Path = strPath;
+            fsw1.IncludeSubdirectories = true;
+            fsw1.NotifyFilter = NotifyFilters.DirectoryName | NotifyFilters.FileName | NotifyFilters.Size | NotifyFilters.LastAccess | NotifyFilters.CreationTime | NotifyFilters.LastWrite;
+            fsw1.Filter = "*.*";
+            fsw1.Changed += new FileSystemEventHandler(OnChanged);
+           
+        }
+
+        private void OnChanged(object source, FileSystemEventArgs e)
+        {
+            //Add to listView1
+            
+            Debug.Print("");
+        }
+
+        private static bool CheckFile(String strFile)
+        {
+            try
+            {
+                using (FileStream StreamIn = File.Open(strFile, FileMode.Open, FileAccess.Read, FileShare.None))
+                {
+                    if (StreamIn.Length > 0)
+                    {
+                        return true;
+                    }
+                    else
+                    {
+                        return false;
+                    }
+                }
+            }
+            catch (Exception Ex)
+            {
+                return false;
+            }
+        }
+    }
+}
